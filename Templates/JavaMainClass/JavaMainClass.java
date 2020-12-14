a17b481c6b131b8f96fd1c5045e598b09eea8f38
Yoham Gabriel Barboza <profe.yoham.br@gmail.com>
2020-12-14 08:16:59 -0600

    Created Java main class template
    block comment and main method.

---
+++ Templates/JavaMainClass/JavaMainClass.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  User ID:           123456
+ *  Last modified:     1/1/2019
+ *  Version:           1.0
+ **************************************************************************** */
+
+public class JavaMainClass {
+    public static void main(String[] args) {
+
+    }
+}
